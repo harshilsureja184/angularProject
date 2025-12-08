1b1b9ffc0cefc3b3b797aa8da1158857366fcbb2
Harshil Sureja <harshilsureja184@gmail.com>
2025-12-08 18:18:19 +0530

    Add new Branch

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,8 +4,10 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  //templateUrl: './app.html',
+  //styleUrl: './app.css'
+  template : `<h1>Hello , How are you ?</h1>`,
+  styles : 'h1{color: red; font-size: 150px;}'
 })
 export class App {
   protected readonly title = signal('angularProject');
