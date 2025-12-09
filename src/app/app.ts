fec55855b1df8508810a45b42f0e904097ebbd26
Harshil Sureja <harshilsureja184@gmail.com>
2025-12-09 18:35:43 +0530

    Create new Components Header , Home , Footer , Contact

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
+import { Home } from './home/home';
+import { Menu } from './menu/menu';
+import { Contact } from './contact/contact';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  //templateUrl: './app.html',
-  //styleUrl: './app.css'
-  template : `<h1>Hello , How are you ?</h1>`,
-  styles : 'h1{color: red; font-size: 150px;}'
+  imports: [RouterOutlet , Header , Footer , Home , Menu , Contact],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angularProject');
